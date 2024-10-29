39a064a48a5138c8131e40a9eeccb37fde6ebe36
Giancarlo <gian.verdum@gmail.com>
2024-10-29 21:19:30 +0000

    Changed database to supabase

--- src/database.py
+++ src/database.py
@@ -4,13 +4,14 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./members.db"
+# Supabase PostgreSQL connection URL
+DATABASE_URL = "postgres://postgres.overbatiwykqflqyuqat:09tywYLvBRcgmkG6@\
+    aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode\
+        =require&supa=base-pooler.x"
 
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Update the database engine to use the Supabase PostgreSQL URL
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base = declarative_base()
 
 
