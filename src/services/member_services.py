f939e39de849aade70d6e552aa5439bb505e7392
Giancarlo <gian.verdum@gmail.com>
2024-10-29 20:35:06 +0000

    Added exception treatment on services/member_services.py

--- src/services/member_services.py
+++ src/services/member_services.py
@@ -22,34 +22,61 @@ def create_member(db: Session, member_data: MemberCreate) -> MemberRead:
         return MemberRead.model_validate(db_member)
     except SQLAlchemyError as e:
         db.rollback()
-        logging.error(f"Database error occurred: {str(e)}")  # Log the error
+        logging.error(f"Database error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail="Database error occurred")
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected server error")
 
 
 def get_members(db: Session, skip: int = 0, limit: int = 10) -> List[MemberRead]:
-    return db.query(Member).offset(skip).limit(limit).all()
+    try:
+        return db.query(Member).offset(skip).limit(limit).all()
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected server error")
 
 
 def get_member_by_id(db: Session, member_id: int) -> Optional[MemberRead]:
-    return db.query(Member).filter(Member.id == member_id).first()
+    try:
+        return db.query(Member).filter(Member.id == member_id).first()
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected server error")
 
 
 def update_member(
     db: Session, member_id: int, updated_data: MemberUpdate
 ) -> Optional[MemberRead]:
-    member = get_member_by_id(db, member_id)
-    if member:
-        for key, value in updated_data.model_dump().items():
-            setattr(member, key, value)
-        db.commit()
-        db.refresh(member)
-    return member
+    try:
+        member = get_member_by_id(db, member_id)
+        if member:
+            for key, value in updated_data.model_dump().items():
+                setattr(member, key, value)
+            db.commit()
+            db.refresh(member)
+        return member
+    except SQLAlchemyError as e:
+        db.rollback()
+        logging.error(f"Database error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Database error occurred")
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected server error")
 
 
 def delete_member(db: Session, member_id: int) -> bool:
-    member = get_member_by_id(db, member_id)
-    if member:
-        db.delete(member)
-        db.commit()
-        return True
-    return False
+    try:
+        member = get_member_by_id(db, member_id)
+        if member:
+            db.delete(member)
+            db.commit()
+            return True
+        return False
+    except SQLAlchemyError as e:
+        db.rollback()
+        logging.error(f"Database error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Database error occurred")
+    except Exception as e:
+        logging.error(f"Unexpected error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected server error")
